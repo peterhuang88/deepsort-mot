58f3d9352d92b86062e99daadec250f2ab4d6707
dhruvswarup123 <dhruvswarup123@gmail.com>
2019-06-21 12:32:00 -0400

    Improving SORT in yolov3

--- yolov3/testingsort.py
+++ yolov3/testingsort.py
@@ -168,31 +168,33 @@ def detect_video(model, args):
                 #for detection in detections:
                 
                 xywh = detections[:,1:5]
+                '''
                 xywh[:, 0] = (detections[:, 1] + detections[:, 3]) / 2
                 xywh[:, 1] = (detections[:, 2] + detections[:, 4]) / 2
                 
                 # TODO: width and hieght are double what they're supposed to be and dunno why
-                xywh[:, 2] = abs(detections[:, 3] - detections[:, 1]) #*2
-                xywh[:, 3] = abs(detections[:, 2] - detections[:, 4]) #*2
+                xywh[:, 2] = abs(detections[:, 3] - detections[:, 1]) *2
+                xywh[:, 3] = abs(detections[:, 2] - detections[:, 4]) *2
+                '''
                 xywh = xywh.cpu().data.numpy() #-> THe final bounding box that can be replaced in the deepSort
-            ######################################################                                
-                print(xywh.shape)
-                print(cls_confs.shape)
-                num_dets, temp = xywh.shape
+                ######################################################                                
+                #print(xywh.shape)
+                #print(cls_confs.shape)
+                #num_dets, temp = xywh.shape
                 #Convert to MOT format
-                xs = xywh[0]
-                ys = xywh[1]
-                ws = xywh[2]
-                hs = xywh[3]
-                new_xs = xs - ws/2
-                new_ys = ys - hs/2
+                xs = xywh[: , 0]
+                ys = xywh[:, 1]
+                ws = xywh[:, 2]
+                hs = xywh[:, 3]
+                #new_xs = xs - ws/2
+                #new_ys = ys - hs/2
                 
                 MOT16_bbox = np.empty((0,5))
                 
                 
-                for cls_id, cls_conf, x,y,w,h in zip(cls_ids, cls_confs, new_xs, new_ys, ws, hs):
+                for cls_id, cls_conf, x,y,w,h in zip(cls_ids, cls_confs, xs, ys, ws, hs):
                     #MOT16_temp = [read_frames, cls_id, x, y, w, h, cls_conf, -1, -1, -1]
-                    MOT16_temp = [x, y, x+w, y+h, cls_conf]
+                    MOT16_temp = [x, y, w, h, cls_conf]
                     np.set_printoptions(precision=2, linewidth=150)
                     MOT16_bbox = np.append(MOT16_bbox, [MOT16_temp], axis=0)
                 """ 
